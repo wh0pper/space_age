bd836491df05b7b1d93bcb30bc2fe273efdf18bf
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-16 11:01:21 -0700

    write test for Age.dateDifference()

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,6 +1,7 @@
 import {Age} from './../src/age';
 
 describe('Age', function() {
+  const secondsInYear = 31536000;
   var testAge;
   var nowObj;
 
@@ -16,7 +17,13 @@ describe('Age', function() {
   });
 
   it('has method .ageToSec that returns current age in seconds', function() {
-    expect(Math.floor(testAge.ageToSec()/31536000)).toEqual(nowObj.getFullYear() - testAge.dob.getFullYear()); //note this test will only pass for the next year
+    expect(Math.floor(testAge.ageToSec()/secondsInYear)).toEqual(nowObj.getFullYear() - testAge.dob.getFullYear()); //note this test will only pass for the next year
+  });
+
+  it('has method .dateDifference that returns difference between two dates in seconds', function() {
+    let date1 = '2017/3/13'
+    let date2 = '03-13-2018'
+    expect(testAge.dateDifference(date1,date2)).toEqual(secondsInYear);
   });
 
 
