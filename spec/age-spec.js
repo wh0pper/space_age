20f133e17b4e5623e8ac36deab47b985e5d19d30
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-16 10:54:49 -0700

    refactor tests for time zone

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -2,19 +2,23 @@ import {Age} from './../src/age';
 
 describe('Age', function() {
   var testAge;
+  var nowObj;
 
   beforeEach(function() {
-    var testDOB = '1990-03-16';
+    var testDOB = '1990-03-13 PST';
     testAge = new Age(testDOB);
+    nowObj = new Date(Date.now());
   });
 
   it('has property containing DOB as a Date object', function() {
     expect(testAge.dob instanceof Date).toEqual(true);
-    expect(testAge.dob.toDateString()).toEqual('Fri Mar 16 1990')
+    expect(testAge.dob.toDateString()).toEqual('Tue Mar 13 1990')
   });
 
-  it('has method .toSec that converts DOB to seconds', function() {
-    expect(testAge.toSec()).toEqual(637545600);
+  it('has method .ageToSec that returns current age in seconds', function() {
+    expect(Math.floor(testAge.ageToSec()/31536000)).toEqual(nowObj.getFullYear() - testAge.dob.getFullYear()); //note this test will only pass for the next year
   });
+
+
 
 });
--- src/age.js
+++ src/age.js
@@ -1,10 +1,11 @@
 export class Age {
   constructor(dob) {
+    const secondsInYear = 31536000;
     this.dob = new Date(dob); //dob expected in yyyy-mm-dd from html form, note this returns
   }
 
-  toSec() {
-    return this.dob.getTime()/1000;
+  ageToSec() {
+    return (Date.now() - Date.parse(this.dob))/1000;
   }
 
 }
